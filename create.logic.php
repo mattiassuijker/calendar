a194b064e1702973dc0a36553be12dcf29250084
mattiassuijker <poldosie@gmail.com>
2016-06-21 11:19:02 +0200

    jaja

--- create.logic.php
+++ create.logic.php
@@ -10,14 +10,16 @@ $result = mysqli_query($db, $sql);
 $birthdays = $result->fetch_all(MYSQLI_ASSOC);
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
+	if (isset($_POST['confirmed'])){
 	$name = $_POST['name'];
 	$day = $_POST['day'];
 	$month =  $_POST['month'];
 	$year = $_POST['year'];
 	$query =  "INSERT INTO birthdays (person, day, month, year) VALUES ('$name', '$day', '$month', '$year')";
 	$result = $db -> query($query);
-
-
-	
 	header('location: index.php');
+	}
+	elseif (isset($_POST['canceled'])){
+		header("location: index.php");
+	}
 }
